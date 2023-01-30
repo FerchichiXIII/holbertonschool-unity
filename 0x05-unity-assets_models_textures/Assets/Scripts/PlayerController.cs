c6b5534bb3328824afc9b3ef63d103f42985bbd8
FerchichiXIII <anasferchichi22@gmail.com>
2023-01-30 13:16:56 +0100

    Add Assets dir

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3f;
+    public float Jump = 3.5f;
+    public CharacterController cc;
+    public GameObject player;
+    private float directionY;
+    private float gravity = 9.0f;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 PlayerMovement = new Vector3(x, 0, z);
+        //transform.Translate(PlayerMovement);
+        if (Input.GetButtonDown("Jump") && cc.isGrounded)
+        {
+            directionY = Jump;
+        }
+        
+        directionY -= gravity * Time.deltaTime;
+        PlayerMovement.y = directionY;
+        cc.Move(PlayerMovement * speed * Time.deltaTime);
+
+        if (transform.position.y < -20)
+        {
+            death();
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            
+        }
+        {
+
+        }
+    }
+    void death()
+    {
+        transform.position = new Vector3(0, 100, 0);     
+    }
+}
