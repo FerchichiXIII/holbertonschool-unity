d5551ec7213a304a090bd30058f10dfae151f2f9
FerchichiXIII <anasferchichi22@gmail.com>
2023-02-10 15:31:18 +0100

    Pushing buttons

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LevelSelect(int level)
+    {
+       SceneManager.LoadScene("Level0" + level.ToString());
+    }
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
