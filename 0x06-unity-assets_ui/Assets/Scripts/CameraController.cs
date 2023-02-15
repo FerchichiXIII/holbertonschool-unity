2e1f9b3ec73f1f7d5ffd3e38e7aa3b4172becf85
FerchichiXIII <anasferchichi22@gmail.com>
2023-02-15 11:21:30 +0100

    Up is down, down is up

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float Speed = 2f;
+    public bool isInverted = false;
     public Transform player;
 
     public Vector3 turn;
@@ -14,14 +15,22 @@ public class CameraController : MonoBehaviour
         turn = transform.position - player.position;
     }
 
-    // Update is called once per frame
+    // Update is called once per fram
     void Update()
     {
+        if (!isInverted)
+        {
+            turn = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Speed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * -Speed, Vector3.left) * turn;
+            transform.position = player.position + turn;
+            transform.LookAt(player.position);
+        }
+        else
+        {
+            turn = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Speed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * Speed, Vector3.left) * turn;
+            transform.position = player.position + turn;
+            transform.LookAt(player.position);
 
-        turn = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Speed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * -Speed, Vector3.left) * turn;
-        transform.position = player.position + turn;
-
-        transform.LookAt(player.position);
+        }
         
     }
 }
